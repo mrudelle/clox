486f662ef6f99663fbbdbc23ba8e4c0621bcfe18
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2022-10-15 09:39:07 +0200

    15.3 - calculator

--- vm.c
+++ vm.c
@@ -30,6 +30,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
     for (;;) {
 
@@ -46,6 +52,11 @@ static InterpretResult run() {
 
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
+            case OP_NEGATE: push(-pop()); break;
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBSTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -61,6 +72,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
