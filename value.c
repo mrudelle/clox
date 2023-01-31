17cca9ce4f85c099e06e52a236f7b8f52b067147
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2023-01-31 23:02:43 +0100

    19.3 - Strings

--- value.c
+++ value.c
@@ -2,6 +2,7 @@
 
 #include "value.h"
 #include "memory.h"
+#include "object.h"
 
 void initValueArray(ValueArray* array) {
     array->count = 0;
@@ -27,11 +28,12 @@ void writeValueArray(ValueArray* array, Value value) {
 
 void printValue(Value value) {
     switch (value.type) {
-    case VAL_BOOL:
-        printf(AS_BOOL(value) ? "true" : "false");
-        break;
-    case VAL_NIL: printf("nil"); break;
-    case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL: printf("nil"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+        case VAL_OBJ: printObject(value); break;
     }
 }
 
